a1098d6891c11f6cbb7d4edca7d955d3e6d397c3
ryuma <info@cortensor.network>
2025-01-06 19:38:33 -0800

    update start script for windows

--- utils/start-win-cygwin.sh
+++ utils/start-win-cygwin.sh
@@ -11,13 +11,14 @@ export PATH=$HOME/.cortensor/bin:$HOME/.cortensor/llm-files:$PATH
 
 cd $HOME/.cortensor
 echo "Starting Cortensor daemon..."
-nohup cortensord .env minerv1 > "$HOME/.cortensor/logs/cortensord.log" 2>&1 &
-PID=$!
+#nohup cortensord .env minerv1 > "$HOME/.cortensor/logs/cortensord.log" 2>&1 &
+#PID=$!
 
-sleep 2
+#sleep 2
 
 # Start the Cortensor daemon
-if ps -p $PID > /dev/null; then
+#if ps -p $PID > /dev/null; then
+if cortensord .env minerv1; then
     echo "Cortensord started successfully."
 else
     echo "Failed to start Cortensord. Please check the logs or environment setup for more details."
@@ -25,4 +26,4 @@ else
 fi
 
 # Save PID to a file for later use
-echo $PID > "$HOME/.cortensor/cortensord.pid"
+# echo $PID > "$HOME/.cortensor/cortensord.pid"
