0632e294238a891bb7942305dcd110dbf02f1b7f
ryuma <info@cortensor.network>
2025-03-28 03:20:04 -0700

    updating register & verify

--- utils/verify.sh
+++ utils/verify.sh
@@ -12,28 +12,6 @@ export PATH=$HOME/.cortensor/bin:$HOME/.cortensor/llm-files:/usr/local/bin/:$PAT
 echo "Navigating to Cortensor configuration directory..."
 cd ~/.cortensor || { echo "Failed to navigate to ~/.cortensor. Directory not found."; exit 1; }
 
-# Check if the .env file exists
-if [[ -f ".env" ]]; then
-    echo "The .env file already exists."
-    # Prompt the user for backup
-    read -p "Do you want to back up the existing .env file before generating a new key? (y/n): " choice
-    case "$choice" in
-        [Yy]* )
-            # Create a backup with a timestamp
-            backup_file=".env.backup.$(date +%Y%m%d%H%M%S)"
-            cp .env "$backup_file"
-            echo "Backup created: $backup_file"
-            ;;
-        [Nn]* )
-            echo "Proceeding without backup..."
-            ;;
-        * )
-            echo "Invalid choice. Exiting."
-            exit 1
-            ;;
-    esac
-fi
-
 # Execute the Cortensor tool to generate a new key
 echo "Verifying using cortensord tool..."
 if cortensord ~/.cortensor/.env tool verify; then
